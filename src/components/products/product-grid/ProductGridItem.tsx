d2b8c05cbc3519caec7231dcf623227bf5643404
karelycode <20041240@itdurango.edu.mx>
2024-06-11 22:28:54 -0600

    chore: Refactor ProductGridItem component and fix image size issue

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -24,19 +24,18 @@ export const ProductGridItem = ({ product }: Props) => {
 
   return (
     <div className="rounded-md overflow-hidden fade-in">
-      <div className=" w-[148px] h-[148px] md:w-[500px] md:h-[500px] flex justify-center items-center bg-gray-100">
-        <Link href={`/product/${product.slug}`}>
-          <Image
-            src={localsrc}
-            alt={product.title}
-            className="object-cover rounded transition-all w-[148px] h-[148px] md:w-[300px] md:h-[360px]"
-            width={300}
-            height={300}
-            onMouseEnter={() => setDisplayImage(product.images[1])}
-            onMouseLeave={() => setDisplayImage(product.images[0])}
-          />
-        </Link>
-      </div>
+      <Link href={`/product/${product.slug}`}>
+        <Image
+          src={localsrc}
+          alt={product.title}
+          className="object-cover rounded transition-all w-[148px] h-[148px] md:w-[500px] md:h-[500px]"
+          width={300}
+          height={300}
+          onMouseEnter={() => setDisplayImage(product.images[1])}
+          onMouseLeave={() => setDisplayImage(product.images[0])}
+        />
+      </Link>
+
       <div className="p-4 flex flex-col">
         <Link href={`/product/${product.slug}`} className="hover:text-blue-600">
           {product.title}
